7ee5295c493e25d615fb601009ad78fa846584d1
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-07-25 20:19:45 +0200

    AuthenticationController partially implemented

---
+++ book-network/src/main/java/com/bullionder/book_network/auth/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.bullionder.book_network.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid RegistrationRequest request
+    ) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
---
+++ book-network/src/main/java/com/bullionder/book_network/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.bullionder.book_network.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+    }
+}
