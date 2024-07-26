b9be313056be7c8a465b710a521fe0fb1b7b58e1
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-07-26 10:06:44 +0200

    BaseEntity parent class implemented

--- book-network/src/main/java/com/bullionder/book_network/book/Book.java
+++ book-network/src/main/java/com/bullionder/book_network/book/Book.java
@@ -1,20 +1,21 @@
 package com.bullionder.book_network.book;
 
+import com.bullionder.book_network.common.BaseEntity;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Book {
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    private Integer id;
     private String title;
     private String authorName;
     private String isbn;
@@ -22,17 +23,4 @@ public class Book {
     private String bookCover;
     private boolean archived;
     private boolean shareable;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-    @LastModifiedDate
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
