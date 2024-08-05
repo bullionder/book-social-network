7d6682b3019952b7588fecfd13e17584ac5da8da
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-08-05 23:35:59 +0200

    feedbacks implemented

---
+++ book-network/src/main/java/com/bullionder/book_network/feedback/FeedbackService.java
@@ -0,0 +1,60 @@
+package com.bullionder.book_network.feedback;
+
+import com.bullionder.book_network.book.Book;
+import com.bullionder.book_network.book.BookRepository;
+import com.bullionder.book_network.common.PageResponse;
+import com.bullionder.book_network.exception.OperationNotPermittedException;
+import com.bullionder.book_network.user.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FeedbackService {
+    private final BookRepository bookRepository;
+    private final FeedbackMapper feedbackMapper;
+    private final FeedbackRepository feedbackRepository;
+
+    public Integer save(FeedbackRequest request, Authentication connectedUser) {
+        Book book = bookRepository.findById(request.bookId())
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID:: " + request.bookId()));
+        if (!book.isShareable() || book.isArchived()) {
+            throw new OperationNotPermittedException("You cannot give a feedback for an archived or not shareable book");
+        }
+
+        User user = (User) connectedUser.getPrincipal();
+        if (!book.getOwner().getId().equals(user.getId())) {
+            throw new OperationNotPermittedException("You cannot give a feedback to your own book");
+        }
+
+        Feedback feedback = feedbackMapper.toFeedback(request);
+        return feedbackRepository.save(feedback).getId();
+    }
+
+    public PageResponse<FeedbackResponse> findAllFeedbacksByBook(Integer bookId, int page, int size, Authentication connectedUser) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createdDate").descending());
+        User user = (User) connectedUser.getPrincipal();
+        Page<Feedback> feedbacks = feedbackRepository.findAllByBookId(bookId, pageable);
+        List<FeedbackResponse> feedbackResponses = feedbacks.stream()
+                .map(feedback -> feedbackMapper.toFeedbackResponse(feedback, user.getId()))
+                .toList();
+
+        return new PageResponse<>(
+                feedbackResponses,
+                feedbacks.getNumber(),
+                feedbacks.getSize(),
+                feedbacks.getTotalElements(),
+                feedbacks.getTotalPages(),
+                feedbacks.isFirst(),
+                feedbacks.isLast()
+        );
+    }
+}
