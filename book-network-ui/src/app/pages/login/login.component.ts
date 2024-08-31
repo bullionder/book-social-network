f6110cb2fcf7ef06631297f688812629221f5736
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-08-31 20:14:36 +0200

    login page partially implemented (created components)

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import {AuthenticationRequest} from "../../services/models/authentication-request";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {register} from "../../services/fn/authentication/register";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgIf,
-    FormsModule
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -19,11 +21,34 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsg: Array<string> = [];
 
-  login() {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
 
+  ) {
+  }
+
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: (res) => {
+        // this.tokenService.token = res.token as string;
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.errorMsg);
+        }
+      }
+    });
   }
 
   register() {
-
+    this.router.navigate(['register'])
   }
 }
