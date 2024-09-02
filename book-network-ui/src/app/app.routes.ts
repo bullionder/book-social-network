5bb08f38e67d31d765f60ad433bfa90fdd48aeab
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-09-02 13:50:47 +0200

    added dummy register component

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
   }
 ];
