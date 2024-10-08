407402061f84c51d39ec76611264f9ce459670d3
Hubert Matlak <hubert.matlak@motorolasolutions.com>
2024-10-08 13:19:34 +0200

    activate account logic partially implemented

---
+++ book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {CodeInputModule} from "angular-code-input";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-activate-account',
+  standalone: true,
+  imports: [
+    CodeInputModule,
+    NgIf
+  ],
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss'
+})
+export class ActivateAccountComponent {
+
+  message = '';
+  isOkay = true;
+  submitted = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login'])
+  }
+
+  private confirmAccount(token: string) {
+    this.authService.confirm({
+      token
+    }).subscribe({
+      next: () => {
+        this.message = 'Your account has been successfully activated.\nNow you can proceed to login'
+        this.submitted = true;
+      },
+      error: () => {
+        this.message = 'Token has been expired or invalid'
+        this.submitted = true;
+        this.isOkay = false;
+      }
+    })
+  }
+}
